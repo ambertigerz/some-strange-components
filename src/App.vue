1a026601ae3a11772f635437d853ab3201e19df6
张小虎 <ambertigerz@zhangyedeMBP.mshome.net>
2022-08-28 14:55:43 +0800

    feat: 0.1.0 project init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import MultiLayer from "./components/MultiLayer/index.vue";
+</script>
+
+<template>
+  <div id="app">
+    <MultiLayer :pics="['1', '2', '3']" />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/MultiLayer/index.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+interface iMultiLayer {
+  pics: Array<string>;
+  width?: number | string;
+  height?: number | string;
+}
+
+withDefaults(defineProps<iMultiLayer>(), {
+  width: "100%",
+  height: "100%",
+});
+</script>
+
+<template>
+  <div>
+    <img
+      v-for="(i, index) in pics"
+      :key="index"
+      :src="i"
+      :alt="index + ''"
+      :style="{ zIndex: 50 - index }"
+    />
+  </div>
+</template>
+
+<style scoped>
+#multi-layer-container {
+  display: flex;
+  box-sizing: border-box;
+  width: v-bind(width);
+  height: v-bind(height);
+}
+</style>
