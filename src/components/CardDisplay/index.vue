36cb55baab352a437241020aa7ee42661d46de6b
Jinghu Zhang <zombiejusthover@gmail.com>
2022-09-04 23:21:22 +0800

    dev

---
+++ src/components/CardDisplay/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="card-display"></div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+</script>
+
+<style lang="less" scoped>
+#card-display{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
