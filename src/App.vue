eff75d55beb6abe9cd7276458261ae3f772e582a
张小虎 <ambertigerz@zhangyedeMBP.mshome.net>
2022-09-07 22:15:40 +0800

    feat: Card Display Dev..

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,47 @@
 <script setup lang="ts">
-import MultiLayer from "./components/MultiLayer/index.vue";
+import { ref } from "vue";
+import CardDisplay from "./components/CardDisplay/index.vue";
+const list = ref([
+  {
+    name: "The Fool",
+    src: "https://img.zcool.cn/community/01661a5f4f0afa11013f1a640f4a0b.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
+  },
+  {
+    name: "Wheel Of Fortune",
+    src: "https://img.zcool.cn/community/01a5165f4f0afa11013e31871df737.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
+  },
+  {
+    name: "The High Priestess",
+    src: "https://img.zcool.cn/community/01fc3f5f4f0afa11013e3187935ca7.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
+  },
+  {
+    name: "Death",
+    src: "https://img.zcool.cn/community/019a575f4f0afa11013f1a64488c3c.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
+  },
+  {
+    name: "Wand",
+    src: "https://img.zcool.cn/community/018b7c5f4f0afa11013e3187d1a54f.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
+  },
+  {
+    name: "Wand2",
+    src: "https://img.zcool.cn/community/01c9775f4f0afa11013f1a644fc288.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
+  },
+  {
+    name: "Pentacle",
+    src: "https://img.zcool.cn/community/01fbd95f4f0afa11013e318702a7ef.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,q_100",
+  },
+]);
 </script>
 
 <template>
   <div id="app">
     <div style="width: 500px; height: 400px">
-      <MultiLayer :pics="[{ layerSrc: '/0.png' }, { layerSrc: '/1.png' }, { layerSrc: '/2.png' }]" />
+      <CardDisplay :cards="list" :display-num="9" />
     </div>
   </div>
 </template>
 
-<style scoped>
+<style lang="less" scoped>
 #app {
   display: flex;
   justify-content: center;
--- src/components/CardDisplay/index.vue
+++ src/components/CardDisplay/index.vue
@@ -1,49 +1,140 @@
 <template>
-    <div id="card-display">
-        <div v-for="(i,index) in cards" :key="i.name" :class="[index,'card']"
-            :style="{height:(200*firstImgIndex-200*index+200)/(displayNum+1)+'%'}">
-            <img :src="i.src">
-        </div>
+  <div id="card-display">
+    <div
+      v-for="(i, index) in cards"
+      :key="i.name"
+      :class="[index, 'card']"
+      :style="{
+        height:
+          100 -
+            (200 * Math.abs(firstImgIndex + (displayNum - 1) / 2 - index)) /
+              (displayNum + 1) >
+          0
+            ? 100 -
+              (200 * Math.abs(firstImgIndex + (displayNum - 1) / 2 - index)) /
+                (displayNum + 1) +
+              '%'
+            : 0,
+        // transform: `translate(${
+        //   20 * firstImgIndex + 10 * (displayNum - 1) - 20 * index
+        // }%)`,
+        zIndex:
+          cards.length +
+          100 -
+          Math.abs(firstImgIndex + (displayNum - 1) / 2 - index),
+        // display:
+        //   100 -
+        //     (200 * Math.abs(firstImgIndex + (displayNum - 1) / 2 - index)) /
+        //       (displayNum + 1) >
+        //   0
+        //     ? `flex`
+        //     : `none`,
+        aspectRatio: 1 / aspectRatio,
+        backgroundImage: `url(${i.src})`,
+        opacity:
+          100 -
+            (200 * Math.abs(firstImgIndex + (displayNum - 1) / 2 - index)) /
+              (displayNum + 1) >
+          0
+            ? 1
+            : 0,
+      }"
+      @click="switchCard(index)"
+    >
+      <div
+        class="card-model"
+        :style="{
+          fontSize:
+            100 -
+            (200 * Math.abs(firstImgIndex + (displayNum - 1) / 2 - index)) /
+              (displayNum + 1) +
+            '%',
+        }"
+      >
+        {{ i.name }}
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref, computed, ComputedRef } from "vue";
 
 interface iCardDisplay {
-    cards: Array<{ src: string, name: string | number, desc?: string | number }>;
-    displayNum?: number;
-    aspectRatio?: number;
-    width?: number | string;
-    height?: number | string;
+  cards: Array<{ src: string; name: string | number; desc?: string | number }>;
+  displayNum?: number;
+  aspectRatio?: number;
+  width?: number | string;
+  height?: number | string;
 }
+
+const props = withDefaults(defineProps<iCardDisplay>(), {
+  width: "100%",
+  height: "100%",
+  displayNum: 5,
+  aspectRatio: 2,
+});
 
 const firstImgIndex = ref<number>(0);
 
-withDefaults(defineProps<iCardDisplay>(), {
-    width: "100%",
-    height: "100%",
-    displayNum: 5,
-    aspectRatio: 2,
-})
+const switchCard = (index: number) => {
+  //   firstImgIndex.value = index - (props.displayNum - 1) / 2;
+  const changeIndex = (type: "+" | "-") => {
+    setTimeout(() => {
+      type === "+" ? firstImgIndex.value++ : firstImgIndex.value--;
+      if (index === firstImgIndex.value + (props.displayNum - 1) / 2) {
+        return;
+      } else {
+        changeIndex(type);
+      }
+    }, 150);
+  };
+  if (index > firstImgIndex.value + (props.displayNum - 1) / 2) {
+    changeIndex("+");
+  } else if (index < firstImgIndex.value + (props.displayNum - 1) / 2) {
+    changeIndex("-");
+  }
+};
 </script>
 
 <style lang="less" scoped>
 #card-display {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: v-bind(width);
+  height: v-bind(height);
+  padding: 10px;
+  box-sizing: border-box;
+
+  .card {
     display: flex;
     justify-content: center;
     align-items: center;
-    width: v-bind(width);
-    height: v-bind(height);
-    padding: 10px;
     box-sizing: border-box;
+    padding: 0;
+    transition: all 0.3s;
+    background-size: 100% 100%;
 
-    .card {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border: 2px solid black;
-        box-sizing: border-box;
+    &:hover {
+      filter: drop-shadow(0 0 20px rgba(255, 250, 190, 0.7));
+      & .card-model {
+        opacity: 1;
+      }
     }
+
+    &-model {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      height: 100%;
+      background: rgba(0, 0, 0, 0.7);
+      color: white;
+      font-weight: bold;
+      transition: all 0.3s;
+      opacity: 0;
+    }
+  }
 }
 </style>
